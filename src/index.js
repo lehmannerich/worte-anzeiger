197e6ff1474065d3d4eae87989538adac648aa79
lehmannerich <lehmannerich@live.de>
2022-07-04 18:05:43 +0200

    Added Word Saver

--- src/index.js
+++ src/index.js
@@ -114,3 +114,55 @@ function getNext() {
     document.getElementById("h1").innerHTML = h1;
   }
 }
+
+
+// Swipe up to Save Word
+
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+                                                                         
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+                                                                         
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* right swipe */ 
+        } else {
+            /* left swipe */
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            /* up swipe */ 
+            document.getElementById("wordbox").innerHTML = '<h1 id="h1">' + h1 + '</h1>';
+        } else { 
+            /* down swipe */
+            console.log("User saved " + h1)
+            document.getElementById("wordbox").innerHTML += '<h2>' + h1 + '</h2>';
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
